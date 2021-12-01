9acfb5a1dc53bd6245071234708966b731617d7b
Rick Noelle <rick@ricknoelle.com>
2021-12-01 17:48:20 -0500

    Added code for part two of day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -20,7 +20,7 @@ fn part_one() {
     let mut _decreases: i32 = 0;
     let mut last: i32 = 0;
 
-    println!("===============================================================");
+    println!("===================== PART 1 ============================");
 
     // Iterate over measurements file line by line
     let reader = get_file_reader("measurements.txt");
@@ -52,17 +52,56 @@ fn part_one() {
         println!("LAST = {}", last);
     }
 
+    println!("TOTALS \nINCREASES = {}\nDECREASES = {}", _increases, _decreases);
     println!("===============================================================");
 
     // print the results
-    print!("TOTALS \nINCREASES = {}\nDECREASES = {}", _increases, _decreases);
+
 }
 
 fn part_two() {
+
+    println!("========================= PART 2 ===============================");
+
     let reader = get_file_reader("measurements.txt");
+
+    // I'm sure there is a better way to do this
+    // Iterate through measures and load them into an array of int 32
+    let mut measurements: Vec<i32> = Vec::new();
     for (index, line) in reader.lines().enumerate() {
-        println!("{}", line.unwrap());
+        // convert string to an int
+        let measurement = line.unwrap();
+        let measurement_int: i32 = measurement.parse().unwrap_or(0);
+        measurements.push(measurement_int);
     }
+
+    let vector_size = measurements.len() as i32;
+    println!("The vector size is {}", vector_size);
+    let window_size: i32 = 3;
+    let mut _increases: i32 = 0;
+    let mut _decreases: i32 = 0;
+    let mut _no_change: i32 = 0;
+    for (index, measure) in measurements.iter().enumerate() {
+        let index_32 = index as i32;
+        if (index_32 < (vector_size - window_size)) {
+            let mut sum_a = measurements[index] + measurements[index+1] + measurements[index+2];
+            let mut sum_b = measurements[index+1] + measurements[index+2] + measurements[index+3];
+            print!("{} vs {} = ", sum_a, sum_b);
+            if sum_b > sum_a {
+                _increases += 1;
+                println!("INCREASE");
+            }
+            if sum_b < sum_a {
+                _decreases += 1;
+                println!("DECREASE");
+            }
+            if sum_b == sum_a {
+                _no_change += 1;
+                println!("NO CHANGE");
+            }
+        }
+    }
+    println!("TOTALS \nINCREASES = {}\nDECREASES = {}\nNO CHANGE = {}", _increases, _decreases, _no_change);
 }
 
 fn main() {
