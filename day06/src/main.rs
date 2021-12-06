418f5b0c8ea1dd8aad4d9cef3e7b6c8bdefa5930
Rick Noelle <rick@ricknoelle.com>
2021-12-06 08:53:38 -0500

    starting part two but still not clear on approach

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -2,17 +2,17 @@ struct Fish {
     timer: i32
 }
 
-fn main() {
 
+fn part_one() {
     let mut fishes: Vec<Fish> = vec![];
 
     // Sample data
-    let fish_timers = [ 3 , 4 , 3, 1, 2 ];
-    let days = 80;
+    //let fish_timers = [ 3 , 4 , 3, 1, 2 ];
+    //let days = 80;
 
     // Real data
     let fish_timers = [ 5,1,5,3,2,2,3,1,1,4,2,4,1,2,1,4,1,1,5,3,5,1,5,3,1,2,4,4,1,1,3,1,1,3,1,1,5,1,5,4,5,4,5,1,3,2,4,3,5,3,5,4,3,1,4,3,1,1,1,4,5,1,1,1,2,1,2,1,1,4,1,4,1,1,3,3,2,2,4,2,1,1,5,3,1,3,1,1,4,3,3,3,1,5,2,3,1,3,1,5,2,2,1,2,1,1,1,3,4,1,1,1,5,4,1,1,1,4,4,2,1,5,4,3,1,2,5,1,1,1,1,2,1,5,5,1,1,1,1,3,1,4,1,3,1,5,1,1,1,5,5,1,4,5,4,5,4,3,3,1,3,1,1,5,5,5,5,1,2,5,4,1,1,1,2,2,1,3,1,1,2,4,2,2,2,1,1,2,2,1,5,2,1,1,2,1,3,1,3,2,2,4,3,1,2,4,5,2,1,4,5,4,2,1,1,1,5,4,1,1,4,1,4,3,1,2,5,2,4,1,1,5,1,5,4,1,1,4,1,1,5,5,1,5,4,2,5,2,5,4,1,1,4,1,2,4,1,2,2,2,1,1,1,5,5,1,2,5,1,3,4,1,1,1,1,5,3,4,1,1,2,1,1,3,5,5,2,3,5,1,1,1,5,4,3,4,2,2,1,3 ];
-    //let days = 80;
+    let days = 80;
 
     // create an array of our fish
     for fish_timer in fish_timers {
@@ -20,7 +20,6 @@ fn main() {
         fishes.push(fish);
     }
     println!("{} TOTAL FISH", fishes.len());
-    let mut day = 0;
 
     // Initial State
     print!("Initial State: ");
@@ -29,8 +28,8 @@ fn main() {
     }
     println!();
 
+    let mut day = 0;
     for x in 0..days {
-
         // A day has passed, decrease the timer
         let mut add_fish_count = 0;
         for (_index, fish) in fishes.iter_mut().enumerate() {
@@ -52,13 +51,33 @@ fn main() {
                 fishes.push(new_fish);
             }
         }
-        print!("After {} days: ", day+1);
-        for (_index, fish) in fishes.iter().enumerate() {
-             print!("{},",fish.timer);
-        }
-        println!();
+        // Only uncomment this if the number of days are small!
+        //print!("After {} days: ", day+1);
+        // for (_index, fish) in fishes.iter().enumerate() {
+        //      print!("{},",fish.timer);
+        // }
+        //println!();
         day += 1;
+        println!("DAY {} COMPLETE", day);
     }
     println!("{} TOTAL FISH", fishes.len());
+}
 
+fn part_two() {
+    let mut fishes: Vec<Fish> = vec![];
+
+    // Sample data
+    let fish_timers = [ 3 , 4 , 3, 1, 2 ];
+    let days = 80;
+
+    // Put the timers into a vector so we can get unique items
+    let mut fish_timers_vec: Vec<i32> = vec![];
+    for i in 0..fish_timers.len() {
+        fish_timers_vec.push(fish_timers[i]);
+    }
+}
+
+fn main() {
+    part_one();
+    part_two();
 }
