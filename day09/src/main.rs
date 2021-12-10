8350830104bcd409eb4097ddece11d9600126cc8
Rick Noelle <rick@ricknoelle.com>
2021-12-10 16:25:18 -0500

    day nine parts one and two

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,69 +1,123 @@
-use std::io::{self, Read};
+use std::{
+    collections::{HashSet, VecDeque},
+    io::{self, Read},
+};
 
-fn part_one() {
-
-    // Load the data directly into a vector of vectors of type i32.
-    // We map the lines to a string, then map the strings to chars
-    // in the second mapping and cast them to int 32s.
-    // really cool to leverage this ability.  starting to appreciate
-    // the features of Rust...
-    let heightmap: Vec<Vec<i32>> = load_data()
-        .lines()
-        .map(|line| {
-        line.chars()
-            .map(|c | c.to_digit(10).unwrap() as i32)
-            .collect()
-    }).collect();
-    //println!("{:?}", heightmap);
-
-    let height = heightmap.len();
-    let width = heightmap[0].len();
-    //println!("W={} H={}", width, height);
-
-    // hold low point for later summing up
-    let mut low_points: Vec<i32> = Vec::new();
-
-    // iterate through the heightmap cell by cell
-    // skip the cell if we find a greater or equal value adjacent
-    for row in 0..height {
-        for col in 0..width {
-            let map_value = heightmap[row][col];
-
-            // compare to row above current row if not on row zero
-            if row > 0 && map_value >= heightmap[row-1][col] {
-                continue;
-            }
-            // compare to row below current row if not on last row
-            if row < height-1 && map_value >= heightmap[row+1][col] {
-                continue;
-            }
-            // compare to column to the left if not on column zero
-            if col > 0 && map_value >= heightmap[row][col-1] {
-                continue
-            }
-            // compare to column to the right if not on last column
-            if col < width-1 && map_value >= heightmap[row][col+1] {
-                continue
-            }
-            // if we made it this far, we found a low point
-            low_points.push(map_value);
-        }
-    }
-    // now total up the low points, adding one to each value
-    let mut total = 0;
-    for low_point in low_points.iter() {
-        total += low_point+1;
-        //print!("{}|", low_point);
-    }
-    println!("\nTOTAL: {}", total);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct BasinPoint {
+    row: usize,
+    col: usize,
 }
 
 fn main() {
-    part_one();
+
+    let heightmap: Vec<Vec<i64>> = get_input()
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as i64)
+                .collect()
+        })
+        .collect();
+
+    let width = heightmap.len();
+    let height = heightmap[0].len();
+
+    let mut low_points: Vec<BasinPoint> = Vec::new();
+    for row in 0..width {
+        for col in 0..height {
+
+            let point_height = heightmap[row][col];
+
+            // check row above current row
+            if row > 0 && point_height >= heightmap[row - 1][col] {
+                continue;
+            }
+
+            // check row below current row
+            if row < width - 1 && point_height >= heightmap[row + 1][col] {
+                continue;
+            }
+
+            // check column to the left
+            if col > 0 && point_height >= heightmap[row][col - 1] {
+                continue;
+            }
+
+            // check column to the right
+            if col < height - 1 && point_height >= heightmap[row][col + 1] {
+                continue;
+            }
+
+            low_points.push(BasinPoint { row: row, col });
+        }
+    }
+    let mut totals = 0;
+    for low_point in low_points.iter() {
+        totals += 1 + (heightmap[low_point.row][low_point.col]);
+    }
+    println!("PART ONE: {}",totals);
+
+    // On to part two
+    let mut basin_sizes: Vec<usize> = Vec::new();
+    for point in low_points.iter() {
+        let mut basin_points: HashSet<BasinPoint> = HashSet::new();
+        let mut queue: VecDeque<BasinPoint> = VecDeque::new();
+        queue.push_back(*point);
+        basin_points.insert(*point);
+
+        while queue.len() > 0 {
+
+            let cur = queue.pop_front().unwrap();
+
+            let row = cur.row;
+            let col = cur.col;
+
+            let mut neighbors: Vec<BasinPoint> = Vec::new();
+
+            // check row above current row
+            if row > 0 {
+                neighbors.push(BasinPoint { row: row - 1, col })
+            }
+
+            // check row below current row
+            if row < width - 1 {
+                neighbors.push(BasinPoint { row: row + 1, col })
+            }
+
+            // check column to the right
+            if col > 0 {
+                neighbors.push(BasinPoint { row: row, col: col - 1 })
+            }
+
+            // check column to the right
+            if col < height - 1 {
+                neighbors.push(BasinPoint { row: row, col: col + 1 })
+            }
+
+            for n in neighbors {
+                if basin_points.contains(&n) || heightmap[n.row][n.col] == 9 {
+                    continue;
+                }
+                queue.push_back(n);
+                basin_points.insert(n);
+            }
+        }
+
+        basin_sizes.push(basin_points.len());
+    }
+
+    //println!("{:?}", basin_sizes);
+    basin_sizes.sort();
+    //println!("{:?}", basin_sizes);
+    basin_sizes.reverse();
+    //println!("{:?}", basin_sizes);
+    println!("PART TWO: {}", basin_sizes[0] * basin_sizes[1] * basin_sizes[2]);
+
 }
 
-fn load_data() -> String {
-    let mut data = String::new();
-    io::stdin().lock().read_to_string(&mut data).unwrap();
-    return data.trim().to_string();
+fn get_input() -> String {
+    let mut input = String::new();
+    io::stdin().lock().read_to_string(&mut input).unwrap();
+    return input.trim().to_string();
 }
